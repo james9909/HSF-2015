cb4f3f1455492384522236a52a740183b6e040f8
James Wang <jameswang9909@hotmail.com>
2015-10-02 18:10:47 -0400

    Solve Git it, Got it, Good

---
+++ Git-it-Got-it-Good_250/Solution.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd git-it-got-it-good
+git checkout master
+
+for i in `seq 1 7`;
+do
+    git checkout HEAD~1
+    cat s3cr3t >> ../s3cr3t
+done
+
+# With a little bit of reconstruction and knowledge about memes, we can reconstruct s3cr3t
+# And his name is flag{JO0O0HHHNNNN_CEEENNNNA4A4A}
