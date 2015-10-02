d0ed27de7817f0378f3b66a62a5dcf7ca90e883c
James Wang <jameswang9909@hotmail.com>
2015-10-01 23:29:10 -0400

    Solve Julius, So Board, and What do you say

---
+++ What-do-you-say_100/Solution.py
@@ -0,0 +1,6 @@
+morse = "doooot dootdootdootdoot dootdoooot dooootdoot dooootdootdoooot dootdootdoot dooootdoooot dootdooootdoot dootdootdoot dooootdootdoooot doot dootdooootdootdoot doot doooot dootdoooot dootdooootdootdoot dooootdootdoot dooootdooootdoooot dooootdooootdoooot doooot dooootdootdoot dooootdooootdoooot dooootdooootdoooot doooot"
+
+print morse.replace("doooot", "-").replace("doot", ".")
+
+# - .... .- -. -.- ... -- .-. ... -.- . .-.. . - .- .-.. -.. --- --- - -.. --- --- -
+# THANKSMRSKELETALDOOTDOOT
