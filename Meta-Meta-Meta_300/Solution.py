35d43eb76747225a9fb460381ffd002743f9aba2
James Wang <jameswang9909@hotmail.com>
2015-10-03 22:46:28 -0400

    Solve Meta Meta Meta

--- Meta-Meta-Meta_300/Solution.py
+++ Meta-Meta-Meta_300/Solution.py
@@ -1,10 +1,11 @@
 from pwn import *
 import requests
-import subprocess
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 def curl_pics():
     url = "http://54.152.5.91/img_0"
-    for x in range(34, 40):
+    for x in range(0, 40):
         if x < 10:
             x = "0" + str(x)
         x = str(x)
@@ -14,33 +15,44 @@ def curl_pics():
                 for chunk in r:
                     f.write(chunk)
 
+def get_exif(fn):
+    ret = {}
+    i = Image.open(fn)
+    info = i._getexif()
+    for tag, value in info.items():
+	decoded = TAGS.get(tag, tag)
+	ret[decoded] = value
+    return ret
+
 def parse_prompt(prompt):
     prompt = prompt.split(": ")
     url = prompt[0]
     filename = url.split("/")[-1]
 
-    data = subprocess.check_output("exiftool -'%s' %s" % (prompt[1].strip(), filename), shell=True)
-    ans = data.split(": ")[1]
-    # subprocess.call("rm pic", shell=True)
+    f = open(filename, "rb")
+    ans = get_exif(f)[prompt[1].strip()]
+
     print ans
-    return ans
+    return str(ans)
 
 # curl_pics()
 def main():
-    try:
-        r = remote('54.152.5.91', 1337)
-        for x in range(5):
-            recv = r.recvline()
-            if "flag" in recv:
-                print recv
-                return
-            print recv
-            prompt = r.recvline()
-            print prompt
-            r.send(parse_prompt(prompt) + "\n")
+    r = remote('54.152.5.91', 1337)
 
-        print r.recvline()
-    except:
-        main()
+    # Need to submit 35 times
+    for x in range(36):
+        recv = r.recvline()
+        if "flag" in recv:
+            print recv
+            return
+        print recv
+        prompt = r.recvline()
+        print prompt
+        r.send(parse_prompt(prompt) + "\n")
+
+    print r.recvline()
 
 main()
+
+# Need to use the PIL module or the tags/values will not be right
+# flag{so_meta_dud3}
